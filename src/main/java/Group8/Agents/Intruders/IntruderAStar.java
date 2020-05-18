b0f80d2abb1455c25372b0a6e05cb7c7b2ef56ee
Tijn Logtens <tijn.logtens@hotmail.nl>
2020-05-18 13:45:40 +0200

    Homing enabled

--- src/main/java/Group8/Agents/Intruders/IntruderAStar.java
+++ src/main/java/Group8/Agents/Intruders/IntruderAStar.java
@@ -13,18 +13,29 @@ import Interop.Percept.IntruderPercepts;
 import Interop.Percept.Smell.SmellPerceptType;
 
 public class IntruderAStar implements Intruder {
-    int i = 0;
+    private double TotalTurn = 0; //total angle between the direction we're looking and the object
+    private double angle = 0; //the angle we will actually turn this turn
 
     @Override
     public IntruderAction getAction(IntruderPercepts percepts) {
-        //System.out.println(percepts.getTargetDirection().getRadians());
-        if(i<=10)
+        TotalTurn = percepts.getTargetDirection().getRadians();
+        System.out.println("To turn = " + TotalTurn);
+        TotalTurn = (double)Math.round(TotalTurn * 100000d) / 100000d;
+
+        if(TotalTurn > Math.toRadians(7))
         {
-            System.out.println("true");
-            i++;
-            return new Rotate(Angle.fromRadians(percepts.getTargetDirection().getRadians()));
+            if(TotalTurn>Math.PI/4){
+                angle = Math.PI/4;
+                System.out.println("Turning: "+angle);
+            }
+            else{
+                angle = TotalTurn;
+            }
+            System.out.println("turned");
+            return new Rotate(Angle.fromRadians(angle));
         }
         else {
+            System.out.println("moved");
             return new Move(new Distance(0.1));
         }
     }
